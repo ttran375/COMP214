f163ad0ed2feec882a80e5fb944c61a93e0f6993
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-02 01:09:49 +0000

    Fri, Feb 2, 2024, 1:09 AM +00:00

--- Week4.sql
+++ Week4.sql
@@ -231,3 +231,15 @@ BEGIN
   DBMS_OUTPUT.PUT_LINE(lv_tax_num);
 END;
 /
+
+DECLARE
+  lv_state_txt CHAR(2) := 'VA';
+  lv_sub_num NUMBER(5,2) := 100;
+  lv_tax_num NUMBER(4,2) := 0;
+BEGIN
+  IF lv_state_txt = 'VA' THEN
+    lv_tax_num := lv_sub_num * 0.06;
+  END IF;
+  DBMS_OUTPUT.PUT_LINE(lv_tax_num);
+END;
+/
