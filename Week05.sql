8f2b6477695a171e60c9b4f1741481d1749cb8c2
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-08 23:03:24 +0000

    Thu, Feb 8, 2024, 11:03 PM +00:00

--- Week05.sql
+++ Week05.sql
@@ -126,3 +126,37 @@ BEGIN
     );
 END;
 
+-- FIGURE 3-12 Including data retrieval and conditional IF processing in a block
+DECLARE
+    lv_state_txt bb_basket.shipstate%TYPE;
+    lv_sub_num bb_basket.subtotal%TYPE;
+    lv_tax_num NUMBER(4,2) := 0;
+BEGIN
+    SELECT 
+        subtotal, 
+        shipstate
+    INTO 
+        lv_sub_num, 
+        lv_state_txt
+    FROM 
+        bb_basket
+    WHERE 
+        idbasket = 4;
+
+    IF lv_state_txt = 'VA' THEN
+        lv_tax_num := lv_sub_num * .06;
+    ELSIF lv_state_txt = 'ME' THEN
+        lv_tax_num := lv_sub_num * .05;
+    ELSIF lv_state_txt = 'NY' THEN
+        lv_tax_num := lv_sub_num * .07;
+    ELSE
+        lv_tax_num := lv_sub_num * .04;
+    END IF;
+
+    DBMS_OUTPUT.PUT_LINE(
+        'ST: ' || lv_state_txt || 
+        ' Sub: ' || lv_sub_num || 
+        ' Tax: ' || lv_tax_num
+    );
+END;
+/
