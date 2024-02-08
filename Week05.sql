d723b105276ca7f6c890c4c49e34a2652bf9069c
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-08 22:42:48 +0000

    Thu, Feb 8, 2024, 10:42 PM +00:00

--- Week05.sql
+++ Week05.sql
@@ -30,7 +30,6 @@ BEGIN
                          || ' * '
                          || lv_days_num);
 END;
-/
 
 DECLARE
     lv_qty_num NUMBER (3);
@@ -44,6 +43,7 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE(lv_qty_num);
 END;
 
+-- FIGURE 3-5 A block checking for an uncompleted basket
 DECLARE
     lv_created_date DATE; lv_basket_num NUMBER (3);
     lv_qty_num NUMBER (3);
@@ -61,4 +61,39 @@ SELECT idBasket, dtcreated, quantity, subtotal
     DBMS_OUTPUT.PUT_LINE (lv_basket_num || ' * ' ||
     TO_CHAR(lv_created_date, 'DD-MON-YYYY') || ' * ' ||
     lv_qty_num || ' * ' || lv_sub_num || ' * ' || lv_days_num);
+END;
+
+-- Using the %TYPE Attribute
+DECLARE
+    lv_basket_num bb_basket.idBasket%TYPE;
+    lv_created_date bb_basket.dtcreated%TYPE;
+    lv_qty_num bb_basket.quantity%TYPE;
+    lv_sub_num bb_basket.subtotal%TYPE;
+    lv_days_num NUMBER(3);
+BEGIN
+    SELECT 
+        idBasket, 
+        dtcreated, 
+        quantity, 
+        subtotal
+    INTO 
+        lv_basket_num, 
+        lv_created_date, 
+        lv_qty_num, 
+        lv_sub_num
+    FROM 
+        bb_basket
+    WHERE 
+        idShopper = 25
+        AND orderplaced = 0;
+
+    lv_days_num := TO_DATE('02/28/12','mm/dd/yy') - lv_created_date;
+
+    DBMS_OUTPUT.PUT_LINE(
+        lv_basket_num || ' * ' ||
+        TO_CHAR(lv_created_date, 'DD-MON-YYYY') || ' * ' || 
+        lv_qty_num || ' * ' ||
+        lv_sub_num || ' * ' || 
+        lv_days_num
+    );
 END;
