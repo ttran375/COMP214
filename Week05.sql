53cbd81a29308111940ecebff72b1a9e83349f6a
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-09 00:08:45 +0000

    Fri, Feb 9, 2024, 12:08 AM +00:00

--- Week05.sql
+++ Week05.sql
@@ -160,3 +160,15 @@ BEGIN
     );
 END;
 /
+
+-- FIGURE 3-14 Performing an INSERT in a block
+DECLARE
+  lv_first_txt bb_shopper.firstname%TYPE := 'Jeffrey';
+  lv_last_txt bb_shopper.lastname%TYPE := 'Brand';
+  lv_email_txt bb_shopper.email%TYPE := 'jbrand@site.com';
+BEGIN
+  INSERT INTO bb_shopper (idshopper, firstname, lastname, email)
+  VALUES (bb_shopper_seq.NEXTVAL, lv_first_txt, lv_last_txt, lv_email_txt);
+  COMMIT;
+END;
+/
