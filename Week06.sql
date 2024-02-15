2c8d6244a98971bdc7ce3aecab11fc8b0d373b7a
Dat Tran <datttrian@gmail.com>
2024-02-15 22:53:07 +0000

    Thu, Feb 15, 2024, 10:53 PM +00:00

--- Week06.sql
+++ Week06.sql
@@ -288,3 +288,22 @@ EXCEPTION
     WHEN ex_prod_update THEN
         DBMS_OUTPUT.PUT_LINE('Invalid product ID entered');
 END;
+
+DECLARE
+  lv_ordqty_num NUMBER (2) := 20;
+  lv_stock_num bb_product.stock%TYPE;
+  ex_prod_stk EXCEPTION;
+BEGIN
+  SELECT stock
+  INTO lv_stock_num
+  FROM bb_product
+  WHERE idProduct = 2;
+
+  IF lv_ordqty_num > lv_stock_num THEN
+    RAISE ex_prod_stk;
+  END IF;
+EXCEPTION
+  WHEN ex_prod_stk THEN
+    DBMS_OUTPUT.PUT_LINE('Requested quantity beyond stock level');
+    DBMS_OUTPUT.PUT_LINE('Req qty = ' || lv_ordqty_num || ' Stock qty= ' || lv_stock_num);
+END;
