70d3c96a4a39dfe0974d1a4424e6421166914b41
Dat Tran <datttrian@gmail.com>
2024-02-15 22:28:19 +0000

    Thu, Feb 15, 2024, 10:28 PM +00:00

--- Week06.sql
+++ Week06.sql
@@ -170,3 +170,46 @@ BEGIN
                              || rec_order.price);
     END LOOP;
 END;
+
+DECLARE
+    cv_prod       SYS_REFCURSOR;
+    rec_item      bb_basketitem%ROWTYPE;
+    rec_status    bb_basketstatus%ROWTYPE;
+    lv_input1_num NUMBER(2) := 2;
+    lv_input2_num NUMBER(2) := 3;
+BEGIN
+    IF lv_input1_num = 1 THEN
+        OPEN cv_prod FOR
+            SELECT
+                *
+            FROM
+                bb_basketitem
+            WHERE
+                idBasket = lv_input2_num;
+        LOOP
+            FETCH cv_prod INTO rec_item;
+            EXIT WHEN cv_prod%NOTFOUND;
+            DBMS_OUTPUT.PUT_LINE(rec_item.idProduct);
+        END LOOP;
+
+        CLOSE cv_prod;
+    ELSIF lv_input1_num = 2 THEN
+        OPEN cv_prod FOR
+            SELECT
+                *
+            FROM
+                bb_basketstatus
+            WHERE
+                idBasket = lv_input2_num;
+        LOOP
+            FETCH cv_prod INTO rec_status;
+            EXIT WHEN cv_prod%NOTFOUND;
+            DBMS_OUTPUT.PUT_LINE(rec_status.idStage
+                                 || ' - '
+                                 || rec_status.dtstage);
+        END LOOP;
+
+        CLOSE cv_prod;
+    END IF;
+END;
+/
