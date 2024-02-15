15e2030436574bada4353feb03eefdb1a0e22c3f
Dat Tran <datttrian@gmail.com>
2024-02-15 22:16:47 +0000

    Thu, Feb 15, 2024, 10:16 PM +00:00

--- Week06.sql
+++ Week06.sql
@@ -35,3 +35,39 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('Rows affected in bb_product: ' || TO_CHAR(SQL%ROWCOUNT));
   END IF;
 END;
+
+DECLARE
+  CURSOR cur_basket IS
+    SELECT b.idBasket, p.type, bi.price, bi.quantity 
+    FROM bb_basketitem bi 
+    INNER JOIN bb_product p USING (idProduct)
+    WHERE bi.idBasket = :g_basket;
+
+  TYPE type_basket IS RECORD (
+    basket bb_basketitem.idBasket%TYPE,
+    type bb_product.type%TYPE,
+    price bb_basketitem.price%TYPE,
+    qty bb_basketitem.quantity%TYPE
+  );
+
+  rec_basket type_basket;
+  lv_rate_num NUMBER(2,2);
+  lv_tax_num NUMBER(4,2) := 0;
+BEGIN
+  OPEN cur_basket;
+  LOOP
+    FETCH cur_basket INTO rec_basket;
+    EXIT WHEN cur_basket%NOTFOUND;
+
+    IF rec_basket.type = 'E' THEN 
+      lv_rate_num := .05; 
+    ELSIF rec_basket.type = 'C' THEN 
+      lv_rate_num := .03; 
+    END IF;
+
+    lv_tax_num := lv_tax_num + ((rec_basket.price * rec_basket.qty) * lv_rate_num);
+  END LOOP;
+  CLOSE cur_basket;
+
+  DBMS_OUTPUT.PUT_LINE('Tax Amount: ' || TO_CHAR(lv_tax_num));
+END;
