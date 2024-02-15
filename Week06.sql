f94a1b0fc53c3739968ca94dba36112e0bb49513
Dat Tran <datttrian@gmail.com>
2024-02-15 22:07:15 +0000

    Thu, Feb 15, 2024, 10:07 PM +00:00

--- Week06.sql
+++ Week06.sql
@@ -11,3 +11,27 @@ BEGIN
     END IF;
 END;
 
+DECLARE
+  lv_tot_num bb_basket.total%TYPE;
+BEGIN
+  SELECT total
+  INTO lv_tot_num
+  FROM bb_basket
+  WHERE idBasket = 12;
+
+  IF SQL%ROWCOUNT = 0 THEN
+    DBMS_OUTPUT.PUT_LINE('Not Found in bb_basket');
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('Rows affected in bb_basket: ' || TO_CHAR(SQL%ROWCOUNT));
+  END IF;
+
+  UPDATE bb_product
+  SET stock = stock + 25
+  WHERE idProduct = 15;
+
+  IF SQL%ROWCOUNT = 0 THEN
+    DBMS_OUTPUT.PUT_LINE('Not Found in bb_product');
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('Rows affected in bb_product: ' || TO_CHAR(SQL%ROWCOUNT));
+  END IF;
+END;
