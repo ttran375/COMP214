89d9415461b80f69fd8615dd63a818865ad2efce
Dat Tran <datttrian@gmail.com>
2024-02-20 00:05:20 +0000

    Tue, Feb 20, 2024, 12:05 AM +00:00

--- Week05/handling-data/06_record_data_type.sql
+++ Week05/handling-data/06_record_data_type.sql
@@ -31,18 +31,3 @@ BEGIN
                           || ' '
                           || lv_days_num );
 END;
-
--- %ROWTYPE Attribute
-DECLARE
-    rec_shopper bb_shopper%ROWTYPE;
-BEGIN
-    SELECT
-        * INTO rec_shopper
-    FROM
-        bb_shopper
-    WHERE
-        idshopper = 25;
-    DBMS_OUTPUT.PUT_LINE(rec_shopper.lastname);
-    DBMS_OUTPUT.PUT_LINE(rec_shopper.address);
-    DBMS_OUTPUT.PUT_LINE(rec_shopper.email);
-END;
---
+++ Week05/handling-data/07_rowtype_attribute.sql
@@ -0,0 +1,14 @@
+-- %ROWTYPE Attribute
+DECLARE
+    rec_shopper bb_shopper%ROWTYPE;
+BEGIN
+    SELECT
+        * INTO rec_shopper
+    FROM
+        bb_shopper
+    WHERE
+        idshopper = 25;
+    DBMS_OUTPUT.PUT_LINE(rec_shopper.lastname);
+    DBMS_OUTPUT.PUT_LINE(rec_shopper.address);
+    DBMS_OUTPUT.PUT_LINE(rec_shopper.email);
+END;
