433dd6b6f3c83ad3a389c55b4d4fedcf9bc6f0d7
Dat Tran <datttrian@gmail.com>
2024-02-20 14:45:58 +0000

    Tue, Feb 20, 2024, 2:45 PM +00:00

--- Week07/Week07.sql
+++ Week07/Week07.sql
@@ -60,3 +60,68 @@ BEGIN
                      || '-'
                      || SUBSTR(p_phone, 7, 4);
 END;
+
+-- Calling a Procedure from another procedure
+CREATE OR REPLACE PROCEDURE ORDER_TOTAL_SP (
+    p_bsktid IN bb_basketitem.idbasket%TYPE,
+    p_cnt OUT NUMBER,
+    p_sub OUT NUMBER,
+    p_ship OUT NUMBER,
+    p_total OUT NUMBER
+) IS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Order total proc called');
+    SELECT
+        SUM(quantity),
+        SUM(quantity * price) INTO p_cnt,
+        p_sub
+    FROM
+        bb_basketitem
+    WHERE
+        idbasket = p_bsktid;
+    SHIP_COST_SP(p_cnt, p_ship);
+    p_total := NVL(p_sub, 0) + NVL(p_ship, 0);
+    DBMS_OUTPUT.PUT_LINE('Order total proc ended');
+END ORDER_TOTAL_SP;
+
+-- DESCRIBE Command
+DESCRIBE order_total_sp
+
+-- Debugging with DBMS_OUTPUT
+DECLARE
+    promo_flag VARCHAR2(1);
+BEGIN
+    FOR rec_purch IN cur_purch LOOP
+        IF rec_purch.sub > 50 THEN
+            promo_flag := 'A';
+        ELSIF rec_purch.sub > 25 THEN
+            promo_flag := 'B';
+        END IF;
+
+        DBMS_OUTPUT.PUT_LINE('Shopper '
+                             || rec_purch.idshopper
+                             || ' has sub '
+                             || rec_purch.sub
+                             || ' and flag = '
+                             || promo_flag);
+        IF promo_flag IS NOT NULL THEN
+            DBMS_OUTPUT.PUT_LINE('Insert processed for shopper');
+            INSERT INTO bb_promolist (
+                idshopper,
+                column_name_for_path,
+                column_name_for_year,
+                promo_flag,
+                some_other_column
+            ) VALUES (
+                rec_purch.idshopper,
+                path,
+                p_year,
+                promo_flag,
+                NULL
+            );
+        END IF;
+    END LOOP;
+
+    COMMIT;
+END;
+
--- Week07/Workspace.sql
+++ Week07/Workspace.sql
@@ -1,11 +1,37 @@
-CREATE OR REPLACE PROCEDURE phone_fmt_sp (
-    p_phone IN OUT VARCHAR2
-) IS
+DECLARE
+    promo_flag VARCHAR2(1); -- Assuming promo_flag is a single character flag
 BEGIN
-    p_phone := '('
-               || SUBSTR(p_phone, 1, 3)
-                  || ')'
-                  || SUBSTR(p_phone, 4, 3)
-                     || '-'
-                     || SUBSTR(p_phone, 7, 4);
+    FOR rec_purch IN cur_purch LOOP
+        IF rec_purch.sub > 50 THEN
+            promo_flag := 'A';
+        ELSIF rec_purch.sub > 25 THEN
+            promo_flag := 'B';
+        END IF;
+
+        DBMS_OUTPUT.PUT_LINE('Shopper '
+                             || rec_purch.idshopper
+                             || ' has sub '
+                             || rec_purch.sub
+                             || ' and flag = '
+                             || promo_flag);
+        IF promo_flag IS NOT NULL THEN
+            DBMS_OUTPUT.PUT_LINE('Insert processed for shopper');
+ -- Assuming path and p_year are variables that should be defined before
+            INSERT INTO bb_promolist (
+                idshopper,
+                column_name_for_path,
+                column_name_for_year,
+                promo_flag,
+                some_other_column
+            ) VALUES (
+                rec_purch.idshopper,
+                path,
+                p_year,
+                promo_flag,
+                NULL
+            );
+        END IF;
+    END LOOP;
+
+    COMMIT;
 END;
