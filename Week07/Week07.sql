5d591f85cedcced9b2b86e412a7518fe77c31792
Dat Tran <datttrian@gmail.com>
2024-02-20 14:58:48 +0000

    Tue, Feb 20, 2024, 2:58 PM +00:00

--- Week07/Week07.sql
+++ Week07/Week07.sql
@@ -156,4 +156,39 @@ BEGIN
 END;
 
 -- Autonomous Transaction
+CREATE OR REPLACE PROCEDURE tc_test_sp2 IS
+    PRAGMA AUTONOMOUS_TRANSACTION;
+BEGIN
+    INSERT INTO bb_testl VALUES (
+        2
+    );
+    COMMIT;
+END tc_test_sp2;
+
+-- RAISE_APPLICATION_ERROR
+CREATE OR REPLACE PROCEDURE stock_ck_sp (
+    p_qty IN NUMBER,
+    p_prod IN NUMBER
+) IS
+    lv_stock_num bb_product.stock%TYPE;
+BEGIN
+    SELECT
+        stock INTO lv_stock_num
+    FROM
+        bb_product
+    WHERE
+        idProduct = p_prod;
+    IF p_qty > lv_stock_num THEN
+        RAISE_APPLICATION_ERROR(-20000, 'Not enough in stock. Requested: '
+                                        || p_qty
+                                        || ' / Stock level: '
+                                        || lv_stock_num);
+    END IF;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('No Stock found.');
+END stock_ck_sp;
+
+-- Remove a Procedure
+DROP PROCEDURE procedure_name;
 
