cd2a6bb72bb80f0766f9459c694622d91c1254ab
Dat Tran <datttrian@gmail.com>
2024-02-22 22:47:52 +0000

    Thu, Feb 22, 2024, 10:47 PM +00:00

--- Workspace.sql
+++ Workspace.sql
@@ -1,18 +1,22 @@
-CREATE OR REPLACE PROCEDURE phone_fmt_sp
-   (p_phone IN OUT VARCHAR2)
-  IS
+-- Calling a Procedure from another procedure
+CREATE OR REPLACE PROCEDURE ORDER_TOTAL_SP (
+    p_bsktid IN bb_basketitem.idbasket%TYPE,
+    p_cnt OUT NUMBER,
+    p_sub OUT NUMBER,
+    p_ship OUT NUMBER,
+    p_total OUT NUMBER
+) IS
 BEGIN
-  p_phone := '(' || SUBSTR(p_phone,1,3) || ')' ||
-                    SUBSTR(p_phone,4,3) || '-' ||
-                    SUBSTR(p_phone,7,4);
-END;
-/
-
-DECLARE
-  v_phone_number VARCHAR2(20);
-BEGIN
-  v_phone_number := '1234567890'; -- Input phone number without formatting
-  phone_fmt_sp(v_phone_number);   -- Call the procedure to format the phone number
-  DBMS_OUTPUT.PUT_LINE('Formatted phone number: ' || v_phone_number);
-END;
-/
+    DBMS_OUTPUT.PUT_LINE('Order total proc called');
+    SELECT
+        SUM(quantity),
+        SUM(quantity * price) INTO p_cnt,
+        p_sub
+    FROM
+        bb_basketitem
+    WHERE
+        idbasket = p_bsktid;
+    SHIP_COST_SP(p_cnt, p_ship);
+    p_total := NVL(p_sub, 0) + NVL(p_ship, 0);
+    DBMS_OUTPUT.PUT_LINE('Order total proc ended');
+END ORDER_TOTAL_SP;
