51b9b98cae0a5d3c7ae17d06434cce4024f26c0a
Dat Tran <datttrian@gmail.com>
2024-02-23 00:24:42 +0000

    Fri, Feb 23, 2024, 12:24 AM +00:00

--- Workspace.sql
+++ Workspace.sql
@@ -1,1 +1,7 @@
+-- Invoking a function from a PL/SQL block
+DECLARE
+    lv_cost_num NUMBER(5, 2);
 BEGIN
+    lv_cost_num := SHIP_CALC_ST(12); -- Corrected function name to match the defined function
+    DBMS_OUTPUT.PUT_LINE(lv_cost_num); -- Added semicolon at the end of the line
+END;
