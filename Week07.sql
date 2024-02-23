2ece7f4cd8b8d9f819fb814bf8ffe7739dde3d74
Dat Tran <datttrian@gmail.com>
2024-02-23 00:21:29 +0000

    Fri, Feb 23, 2024, 12:21 AM +00:00

--- Week07.sql
+++ Week07.sql
@@ -17,3 +17,30 @@ BEGIN
     v_amt2 := ROUND(v_amt1, 0);
     DBMS_OUTPUT.PUT_LINE(v_amt2);
 END;
+
+-- Creating a function
+CREATE OR REPLACE FUNCTION SHIP_CALC_ST (
+    P_QTY IN NUMBER
+) RETURN NUMBER IS
+    lv_ship_num NUMBER := 0;
+BEGIN
+    IF P_QTY > 10 THEN
+        lv_ship_num := 11.00;
+    ELSIF P_QTY > 5 THEN
+        lv_ship_num := 8.00;
+    ELSE
+        lv_ship_num := 5.00;
+    END IF;
+
+    RETURN lv_ship_num;
+END SHIP_CALC_ST;
+
+-- Invoking a function from a PL/SQL block
+DECLARE
+    lv_cost_num NUMBER(5, 2);
+BEGIN
+    lv_cost_num := SHIP_CALC_ST(12); -- Corrected function name to match the defined function
+    DBMS_OUTPUT.PUT_LINE(lv_cost_num); -- Added semicolon at the end of the line
+END;
+
+-- Invoking a function by name
--- Workspace.sql
+++ Workspace.sql
@@ -1,8 +1,1 @@
-DECLARE
-    v_amt1 NUMBER(5, 2);
-    v_amt2 NUMBER(3, 0);
 BEGIN
-    v_amt1 := 32.50;
-    v_amt2 := ROUND(v_amt1, 0);
-    DBMS_OUTPUT.PUT_LINE(v_amt2);
-END;
