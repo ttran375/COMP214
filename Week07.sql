5c15999354e3fc814d9938c98a4a521cc3d59180
Dat Tran <datttrian@gmail.com>
2024-02-23 00:43:54 +0000

    Fri, Feb 23, 2024, 12:43 AM +00:00

--- Week07.sql
+++ Week07.sql
@@ -91,3 +91,31 @@ FROM
     bb_basket
 WHERE
     orderplaced = 1;
+
+-- Building and Testing a Function...
+CREATE OR REPLACE FUNCTION memfmt1_sf (
+    p_id IN NUMBER,
+    p_first IN VARCHAR2,
+    p_last IN VARCHAR2
+) RETURN VARCHAR2 IS
+    lv_mem_txt VARCHAR2(35);
+BEGIN
+    lv_mem_txt := 'Member '
+                  ||p_id
+                  ||' - '
+                  ||p_first
+                  ||'
+'
+                  ||p_last;
+    RETURN lv_mem_txt;
+END;
+/
+
+DECLARE
+    v_result VARCHAR2(100);
+BEGIN
+    v_result := MEMFMT1_SF(123, 'John', 'Doe');
+    DBMS_OUTPUT.PUT_LINE('Formatted String: '
+                         || v_result);
+END;
+/
--- Workspace.sql
+++ Workspace.sql
@@ -1,8 +1,26 @@
-SELECT
-    SUM(shipping)                               AS actual,
-    SUM(ship_calc_sf(quantity))                 AS calc,
-    SUM(shipping) - SUM(ship_calc_sf(quantity)) AS diff
-FROM
-    bb_basket
-WHERE
-    orderplaced = 1;
+CREATE OR REPLACE FUNCTION memfmt1_sf (
+    p_id IN NUMBER,
+    p_first IN VARCHAR2,
+    p_last IN VARCHAR2
+) RETURN VARCHAR2 IS
+    lv_mem_txt VARCHAR2(35);
+BEGIN
+    lv_mem_txt := 'Member '
+                  ||p_id
+                  ||' - '
+                  ||p_first
+                  ||'
+'
+                  ||p_last;
+    RETURN lv_mem_txt;
+END;
+/
+
+DECLARE
+    v_result VARCHAR2(100);
+BEGIN
+    v_result := MEMFMT1_SF(123, 'John', 'Doe');
+    DBMS_OUTPUT.PUT_LINE('Formatted String: '
+                         || v_result);
+END;
+/
