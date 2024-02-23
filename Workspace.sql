c9a31ebd218bd35fe6f5bcad34862b587e32c691
Dat Tran <datttrian@gmail.com>
2024-02-23 00:31:12 +0000

    Fri, Feb 23, 2024, 12:31 AM +00:00

--- Workspace.sql
+++ Workspace.sql
@@ -2,6 +2,6 @@
 DECLARE
     lv_cost_num NUMBER(5, 2);
 BEGIN
-    lv_cost_num := SHIP_CALC_ST(12); -- Corrected function name to match the defined function
+    lv_cost_num := SHIP_CALC_ST(5); -- Corrected function name to match the defined function
     DBMS_OUTPUT.PUT_LINE(lv_cost_num); -- Added semicolon at the end of the line
 END;
