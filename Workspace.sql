0a7b79a0ebb6442b0b8896cd16b2873aecc503a8
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-07 22:27:28 +0000

    Thu, Mar 7, 2024, 10:27 PM +00:00

--- Workspace.sql
+++ Workspace.sql
@@ -1,7 +1,23 @@
--- Package Specification
-CREATE OR REPLACE PACKAGE metric_pkg IS
-  cup_to_liter CONSTANT NUMBER := .24;
-  pint_to_liter CONSTANT NUMBER := .47;
-  qrt_to_liter CONSTANT NUMBER := .95;
-END;
+-- Forward Declarations
+CREATE OR REPLACE PACKAGE BODY ordering_pkg IS
 
+    FUNCTION ship_calc_pf(
+        p_qty IN NUMBER
+    ) RETURN NUMBER IS
+    BEGIN
+ -- Implementation of ship_calc_pf function goes here
+        RETURN 0; -- Placeholder return value
+    END ship_calc_pf;
+
+    PROCEDURE order_total_pp(
+        p_bsktid IN bb_basketitem.idbasket,
+        p_cnt OUT NUMBER,
+        p_sub OUT NUMBER,
+        p_ship OUT NUMBER,
+        p_total OUT NUMBER
+    ) IS
+    BEGIN
+ -- Implementation of order_total_pp procedure goes here
+        NULL; -- Placeholder implementation
+    END order_total_pp;
+END ordering_pkg;
