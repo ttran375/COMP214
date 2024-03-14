928b86f588017d87b37d9e986220ea349bc7f771
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-14 21:06:46 +0000

    Thu, Mar 14, 2024, 9:06 PM +00:00

--- Week10/Workspace.sql
+++ Week10/Workspace.sql
@@ -11,3 +11,12 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('This is a basic stored procedure.');
 END;
 /
+
+SELECT
+    referenced_name,
+    referenced_type,
+    referenced_owner
+FROM
+    user_dependencies
+WHERE
+    name = 'ORDER_TOTAL_SP';
--- Workspace.sql
+++ Workspace.sql
@@ -1,13 +1,8 @@
 SELECT
-    object_name,
-    status
+    referenced_name,
+    referenced_type,
+    referenced_owner
 FROM
-    user_objects
+    user_dependencies
 WHERE
-    object_name = 'ORDER_TOTAL_SP';
-
-CREATE OR REPLACE PROCEDURE ORDER_TOTAL_SP AS
-BEGIN
-    DBMS_OUTPUT.PUT_LINE('This is a basic stored procedure.');
-END;
-/
+    name = 'ORDER_TOTAL_SP';
