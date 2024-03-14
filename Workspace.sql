7b919a4f63b720f09e2758fd0bd82bacb258b910
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-14 21:03:35 +0000

    Thu, Mar 14, 2024, 9:03 PM +00:00

--- Workspace.sql
+++ Workspace.sql
@@ -5,3 +5,9 @@ FROM
     user_objects
 WHERE
     object_name = 'ORDER_TOTAL_SP';
+
+CREATE OR REPLACE PROCEDURE ORDER_TOTAL_SP AS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('This is a basic stored procedure.');
+END;
+/
